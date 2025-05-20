4767a38df9d3757beacdd9d4e1ad381f39ae15f0
catastrosansebastian <catastrosansebastian2023@gmail.com>
2025-05-19 19:34:09 -0500

    first commit

---
+++ pointclouds/portal/cloud.js
@@ -0,0 +1,26 @@
+{
+    "version": "1.7",
+    "octreeDir": "data",
+    "projection": "+proj=utm +zone=18 +south +datum=WGS84 +units=m +no_defs",
+    "points": 26735595,
+    "boundingBox": {
+        "lx": 819642.293548584,
+        "ly": 8523313.868408204,
+        "lz": 3044.842124938965,
+        "ux": 820100.7151794434,
+        "uy": 8523772.290039063,
+        "uz": 3503.26375579834
+    },
+    "tightBoundingBox": {
+        "lx": 819642.294,
+        "ly": 8523313.868,
+        "lz": 3044.842,
+        "ux": 819955.234,
+        "uy": 8523772.29,
+        "uz": 3147.537
+    },
+    "pointAttributes": "LAZ",
+    "spacing": 3.97004771232605,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
